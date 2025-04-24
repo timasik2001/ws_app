a12ac4ad2403a54f7f22676a9415fc236e864a83
cwdt176 <cwtd176@gmail.com>
2025-04-24 20:44:36 +0300

    init

---
+++ main.py
@@ -0,0 +1,41 @@
+import asyncio
+
+import django
+
+def setup(set_prefix=True):
+    """
+    Configure the settings (this happens as a side effect of accessing the
+    first setting), configure logging and populate the app registry.
+    Set the thread-local urlresolvers script prefix if `set_prefix` is True.
+    """
+    from django.apps import apps
+    from django.conf import settings
+    from django.urls import set_script_prefix
+    from django.utils.log import configure_logging
+    settings.configure()
+    configure_logging(settings.LOGGING_CONFIG, settings.LOGGING)
+    if set_prefix:
+        set_script_prefix(
+            "/" if settings.FORCE_SCRIPT_NAME is None else settings.FORCE_SCRIPT_NAME
+        )
+    apps.populate(settings.INSTALLED_APPS)
+
+
+setup()
+from websockets.asyncio.server import serve
+from websockets.frames import CloseCode
+
+
+async def handler(websocket):
+    sesame = await websocket.recv()
+
+    await websocket.send(f"Hello!")
+
+
+async def main():
+    async with serve(handler, "0.0.0.0", 8888) as server:
+        await server.serve_forever()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
